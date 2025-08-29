f222ddc45d370331ad097d26e27f02487a82a0a5
gautam-18 <gautamarora.1805@gmail.com>
2025-08-29 11:19:03 +0530

    feat: added personal details

--- src/routes/bfhlRoutes.ts
+++ src/routes/bfhlRoutes.ts
@@ -3,6 +3,10 @@ import { handleBfhlPost } from "../controllers/bfhlController";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("Gautam Arora - 22BDS0010");
+});
+
 router.post("/bfhl", handleBfhlPost);
 
 export default router;
